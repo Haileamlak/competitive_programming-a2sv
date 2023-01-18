3eaee5d18cb81ac9d2221fcc6f95ceadd501eb17
Haileamlak <99148511+Haileamlak@users.noreply.github.com>
2023-01-18 18:31:31 +0300

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,9 +1,10 @@
 
 void countSwaps(vector<int> a) {
     int sw=0;
-    for (int i = 0; i < a.size(); i++) {
+    int size = a.size();
+    for (int i = 0; i <size ; i++) {
     
-    for (int j = 0; j < a.size()-1; j++) {
+    for (int j = 0; j < size-1; j++) {
         // Swap adjacent elements if they are in decreasing order
         if (a[j] > a[j + 1]) {
             swap(a[j], a[j + 1]);
@@ -14,5 +15,5 @@ void countSwaps(vector<int> a) {
 }
     cout<<"Array is sorted in "<<sw<<" swaps."<<endl;
     cout<<"First Element: "<<a[0]<<endl;
-    cout<<"Last Element: "<<a[a.size()-1];
+    cout<<"Last Element: "<<a[size-1];
 }
