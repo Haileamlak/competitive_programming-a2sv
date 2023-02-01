37b8f2a7f15effe462b50198ad8423749ee67679
Haileamlak <99148511+Haileamlak@users.noreply.github.com>
2023-02-01 21:19:13 +0300

    Create CountingSort.cpp

---
+++ CountingSort.cpp
@@ -0,0 +1,10 @@
+vector<int> countingSort(vector<int> arr)
+{
+    vector<int> newArr(100);
+    int size = arr.size();
+    for (size_t i = 0; i < size; i++)
+    {
+        newArr[arr[i]]++;
+    }
+    return newArr;
+}
