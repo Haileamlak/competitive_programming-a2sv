f26aa969c5ecb59e15d5b083df2316e4cc214b78
Haileamlak <haileamlakbeat@gmail.com>
2023-10-31 06:39:13 -0700

    Update CountingSort.cpp

--- CountingSort.cpp
+++ CountingSort.cpp
@@ -2,7 +2,7 @@ vector<int> countingSort(vector<int> arr)
 {
     vector<int> newArr(100);
     int size = arr.size();
-    for (size_t i = 0; i < size; i++)
+    for (size_t i = 0;i < size; i++)
     {
         newArr[arr[i]]++;
     }
