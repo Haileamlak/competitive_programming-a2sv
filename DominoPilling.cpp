5cdeefedbf362733da70bc113a1aed18f0d70c38
Haileamlak <haileamlakbeat@gmail.com>
2023-11-02 02:59:38 -0700

    Update DominoPilling.cpp

--- DominoPilling.cpp
+++ DominoPilling.cpp
@@ -6,7 +6,7 @@ int main(){
 
     int m, n;
     cin >> m >> n;
-    if(m<1&&m>16&&n<1&&n>16&&m>n)
+    if(m < 1 && m > 16 && n < 1 && n > 16 && m > n)
         exit(1);
     cout << m * n / 2;
     return 0;
