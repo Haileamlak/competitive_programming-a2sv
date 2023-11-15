719f60ecfeb7cc9f53bcca808eb56864c8d31c7f
Haileamalk <haileamlakbeat@gmail.com>
2023-11-14 22:17:22 -0800

    Update Missing_Number.cpp

--- Missing_Number.cpp
+++ Missing_Number.cpp
@@ -6,7 +6,7 @@ public:
         sort(nums.begin(), nums.end());
 
         int len = nums.size();
-        for ( int i = 0; i < len; i++)
+        for (int i = 0; i < len; i++)
         {
             if(nums[i] != i)
                 return i;
