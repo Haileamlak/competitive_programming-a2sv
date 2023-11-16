f97c83ed29783d088000a590c35668b35c518d79
Haileamalk <haileamlakbeat@gmail.com>
2023-11-16 00:22:37 -0800

    Update NumberOfGoodPairs.cpp

--- NumberOfGoodPairs.cpp
+++ NumberOfGoodPairs.cpp
@@ -4,7 +4,7 @@ public:
         int res = 0;
         for(int i = 0;i < nums.size();i++)
             for(int j = i+1;j < nums.size();j++)
-                if(nums[j]==nums[i])
+                if(nums[j] == nums[i])
                     res++;
         return res;
     }
