8741bf96f032ee5f7c6389cb21cc63f5c45599dd
Haileamalk <haileamlakbeat@gmail.com>
2023-11-19 23:06:01 -0800

    Update areNumbersAscending.cpp

--- areNumbersAscending.cpp
+++ areNumbersAscending.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool areNumbersAscending(string s) {
        stringstream strS (s);
-       int num1 = 0;string iterate;
+       int num1 = 0;
+       string iterate;
        while(strS>>iterate){
-        if( iterate[0]<='9'){
-            if(stoi(iterate) <= num1)
-                return false;
-            num1 = stoi(iterate);
-        }
+           if( iterate[0] <= '9'){
+               if(stoi(iterate) <= num1)
+                   return false;
+               num1 = stoi(iterate);
+           }
        }
        return true;
     }
