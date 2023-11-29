0b027dd32e8ce23a1665b623351a1e4e7cddcf81
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-11-29 00:49:07 -0800

    "practicing data types and variables"

---
+++ python_track/variables_and_data_types.py
@@ -0,0 +1,13 @@
+character_name = 'Abebe'
+character_age = 21
+is_male = True
+identity = "man" if is_male else "woman"
+
+print("There once was a " +  identity +" called " + character_name + ", ")
+print("He was " + str(character_age) + " years old.")
+
+character_name = "Haileamlak"
+character_age = 30
+print("He really liked the name " + character_name + ", ")
+print("But didn't like being " + str(character_age) + ".")
+
