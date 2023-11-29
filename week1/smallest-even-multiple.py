41d6d183aeb5be139b204b723d133913ee616761
Haileamalk <haileamlakbeat@gmail.com>
2023-11-29 06:36:22 -0800

    Added solution for Smallest Even Multiple

--- week1/smallest-even-multiple.py
+++ week1/smallest-even-multiple.py
@@ -1,10 +1,6 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        if(n == 1):
-            return 2
-        
-        x = n
-        while(True):
-            if(x % 2 == 0 and x % n == 0):
-                return x
-            x += 1
+        if(n % 2 == 0):
+            return n
+            
+        return n * 2
