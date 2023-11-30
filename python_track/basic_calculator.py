4397ba5d68daa4be19ecc3366891d2229c6dd28e
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-11-30 01:05:37 -0800

    building basic calculator

---
+++ python_track/basic_calculator.py
@@ -0,0 +1,15 @@
+num1_str = input("Enter a number sir : ")
+num2_str = input("Enter another sir : ")
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print(f"Sum of {num1} and {num2} = {sum}")
+print(f"Difference of {num1} and {num2} = {diff}")
+print(f"Product of {num1} and {num2} = {product}")
+print(f"Quotient of {num1} and {num2} = {quotient}")
