2de8b6d2ebe277236597ea966be0ffe5ac5bea76
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-11-30 02:04:16 -0800

    practicing with functions

---
+++ python_track/function.py
@@ -0,0 +1,10 @@
+def say_hi(name, job):
+    print(f"Hello {name}! You are {job}.")
+
+say_hi("Haileamlak", "Student")
+
+def cube(num):
+    return num ** 3
+
+cube_of_three = cube(3)
+print(cube_of_three)
