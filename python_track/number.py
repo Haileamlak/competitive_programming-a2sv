05cf69637a14d6f2675b22155a6e11bc7430ffd8
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-11-30 00:47:49 -0800

    messing with numbers

---
+++ python_track/number.py
@@ -0,0 +1,12 @@
+from math import *
+
+my_num = 5
+print(5)
+print(my_num + 5)
+print(my_num - 3 * 10)
+print(my_num / 2)
+print(my_num // 2)
+print(my_num % 2)
+print(str(my_num))
+print(abs(-5))
+print(ceil(5.5))
