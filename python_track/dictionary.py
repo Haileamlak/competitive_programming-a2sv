e8a4f9ca0d8fe9cde4a47a31867454fa30c03f0f
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-11-30 21:56:22 -0800

    working with dictionary

---
+++ python_track/dictionary.py
@@ -0,0 +1,17 @@
+month_map = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "July",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(month_map['Jan'])
+print(month_map.get('May', "no may"))
