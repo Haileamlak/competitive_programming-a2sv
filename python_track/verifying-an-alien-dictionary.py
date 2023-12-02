57a5820cbfad05608df14e883a590f7a60def8d6
Haileamalk <haileamlakbeat@gmail.com>
2023-12-02 06:23:02 -0800

    Added solution for Verifying an Alien Dictionary

--- python_track/verifying-an-alien-dictionary.py
+++ python_track/verifying-an-alien-dictionary.py
@@ -1,22 +1,22 @@
 class Solution:   
-    def areSorted(self, str1, str2, order):
-        if str2 == '' and str1 == '':
-            return True
-        if str2 == '' and str1 != '':
-            return False
-        elif str1 == '' and str2 != '':
-            return True
-        elif order.find(str1[0]) > order.find(str2[0]):
-            return False
-        elif order.find(str1[0]) == order.find(str2[0]):
-            return self.areSorted(str1[1:], str2[1:], order)
-        else:
-            return True
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         for i in range(len(words) - 1):
             if not self.areSorted(words[i], words[i + 1], order):
                 return False
         
         return True
+
+    def areSorted(self, str1, str2, order):
+        if str1 == '':
+            return True
+        if str2 == '':
+            return False
+        elif order.find(str1[0]) > order.find(str2[0]):
+            return False
+        elif order.find(str1[0]) == order.find(str2[0]):
+            return self.areSorted(str1[1:], str2[1:], order)
+        else:
+            return True
+            
 
 
