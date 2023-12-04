1cdf49415ef271621d19025eac7cddad9ae1d30c
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-04 08:02:01 -0800

    Added solution for Watering Plants

---
+++ python_track/watering-plants.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        i = -1
+        steps = 0
+        can = capacity
+        while i < len(plants) - 1:
+            if can >= plants[i + 1]:
+                steps += 1
+                can -= plants[i + 1]
+                i += 1
+            else:
+                can = capacity
+                steps += (i + 1) * 2 + 1
+                can -= plants[i + 1]
+                i += 1
+        
+        return steps
+                
