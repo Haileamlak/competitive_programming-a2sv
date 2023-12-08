9e029af58c5870bd4d9a903180070764f9ed5c5a
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-08 08:42:19 -0800

    Added solution for Subdomain Visit Count

---
+++ python_track/subdomain-visit-count.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subdomainVisits(self, cp_domains: List[str]) -> List[str]:
+        domain_visit = {}
+        for cp_domain in cp_domains:
+            cp, domain = cp_domain.split(" ")
+            
+            for i in range(len(domain) - 1, -1, -1):
+                if domain[i] == '.':
+                    domain_visit[domain[i + 1:]] = domain_visit.get(domain[i + 1:], 0) + int(cp)
+            # for the whole domain
+            domain_visit[domain] = domain_visit.get(domain, 0) + int(cp)
+        
+        result = []
+        for domain, cp in domain_visit.items():
+            result.append(str(cp) + " " + domain)
+        
+        return result
