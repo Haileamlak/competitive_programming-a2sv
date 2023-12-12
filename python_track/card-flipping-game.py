dc52dcd6ffc695277e4326f7926dcfc08c8cb05e
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-12 07:00:45 -0800

    Added solution for Card Flipping Game

--- python_track/card-flipping-game.py
+++ python_track/card-flipping-game.py
@@ -1,37 +1,50 @@
 class Solution:
     def flipgame(self, fronts: List[int], backs: List[int]) -> int:
-        front_map = {}
+        s = set()
         for i in range(len(fronts)):
-            if fronts[i] in front_map:
-                front_map[fronts[i]].add(i)
-            else:
-                temp = {i}
-                front_map[fronts[i]] = temp
-        print(front_map)
-        back_map = {}
-        for i in range(len(backs)):
-            if backs[i] in back_map:
-                back_map[backs[i]].add(i)
-            else:
-                temp = {i}
-                back_map[backs[i]] = temp
+            if fronts[i] == backs[i]:
+                s.add(fronts[i])
+                
+        ans = 2001
+        for i in range(len(fronts)):
+            if fronts[i] not in s:
+                ans = min(ans, fronts[i])
+            if backs[i] not in s:
+                ans = min(ans, backs[i])
         
-        res = 10001
-        for i in range(len(backs)):
-            if backs[i] not in front_map:
-                res = min(res, backs[i])
-            elif len(front_map[backs[i]] & back_map[backs[i]]) == 0: # (i not in front_map[backs[i]])
-                res = min(res, backs[i])
+        return ans if ans != 2001 else 0
+        # front_map = {}
+        # for i in range(len(fronts)):
+        #     if fronts[i] in front_map:
+        #         front_map[fronts[i]].add(i)
+        #     else:
+        #         temp = {i}
+        #         front_map[fronts[i]] = temp
 
-        for i in range(len(fronts)):
-            if fronts[i] not in back_map:
-                res = min(res, fronts[i])
-            elif len(back_map[fronts[i]] & front_map[fronts[i]]) == 0: # (i not in back_map[fronts[i]]) 
-                res = min(res, fronts[i])
+        # back_map = {}
+        # for i in range(len(backs)):
+        #     if backs[i] in back_map:
+        #         back_map[backs[i]].add(i)
+        #     else:
+        #         temp = {i}
+        #         back_map[backs[i]] = temp
         
-        if res == 10001:
-            return 0
-        return res
+        # res = 10001
+        # for i in range(len(backs)):
+        #     if backs[i] not in front_map:
+        #         res = min(res, backs[i])
+        #     elif len(front_map[backs[i]] & back_map[backs[i]]) == 0: # (i not in front_map[backs[i]])
+        #         res = min(res, backs[i])
+
+        # for i in range(len(fronts)):
+        #     if fronts[i] not in back_map:
+        #         res = min(res, fronts[i])
+        #     elif len(back_map[fronts[i]] & front_map[fronts[i]]) == 0: # (i not in back_map[fronts[i]]) 
+        #         res = min(res, fronts[i])
+        
+        # if res == 10001:
+        #     return 0
+        # return res
         
 
             
