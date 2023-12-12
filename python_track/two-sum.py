c4f40d03e2af469b4a896182021c9735bca72453
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-12 07:55:59 -0800

    Added solution for Two Sum

--- python_track/two-sum.py
+++ python_track/two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         nums_index = {}
         for i in range(len(nums)):
             num2 = target - nums[i]
-            if num2 in nums_index and nums_index[num2] != i:
+            if num2 in nums_index:
                 return [i, nums_index[num2]]
             
             nums_index[nums[i]] = i
