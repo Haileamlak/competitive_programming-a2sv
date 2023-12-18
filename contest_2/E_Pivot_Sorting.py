3c30e01eba85c0e26f4f56b155f5bdc3bb0df403
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-18 08:15:08 -0800

    added solutions for contest 3

--- contest_2/E_Pivot_Sorting.py
+++ contest_2/E_Pivot_Sorting.py
@@ -1,18 +1,19 @@
-t = int(input())
+# t = int(input())
 
-from math import ceil
-for j in range(t):
-    n = int(input())
-    nums = list(map(int, input().split()))
-    num_set = set()
-    x = [0, 10 ** 9]
-    for i in range(0, len(nums) - 1):
-        if nums[i] > nums[i + 1]:
-            x[0] = max(x[0], int(ceil((nums[i] + nums[i + 1]) / 2)))
-        elif nums[i] < nums[i + 1]:
-            x[1] = min(x[1], (nums[i] + nums[i + 1]) // 2)
+# from math import ceil
+# for j in range(t):
+#     n = int(input())
+#     nums = list(map(int, input().split()))
+#     num_set = set()
+#     x = [0, 10 ** 9]
+#     for i in range(0, len(nums) - 1):
+#         if nums[i] > nums[i + 1]:
+#             x[0] = max(x[0], int(ceil((nums[i] + nums[i + 1]) / 2)))
+#         elif nums[i] < nums[i + 1]:
+#             x[1] = min(x[1], (nums[i] + nums[i + 1]) // 2)
     
-    if x[0] <= x[1]:
-        print(x[0])
-    else:
-        print(-1)
+#     if x[0] <= x[1]:
+#         print(x[0])
+#     else:
+#         print(-1)
+print([chr(91 + i) for i in range(6)])
--- python_track/practice.py
+++ python_track/practice.py
@@ -7,8 +7,25 @@
 # my_list = {x**2 for x in range(10) if  x % 2}
 # print(my_list)
 
-original_dict = {'key1': ['value1'], 'key2': 'value2'}
-shallow_copied_dict = original_dict.copy()
-original_dict['key1'].append('value3')
-original_dict['key2'] = "value4"
-print(shallow_copied_dict)
+# original_dict = {'key1': ['value1'], 'key2': 'value2'}
+# shallow_copied_dict = original_dict.copy()
+# original_dict['key1'].append('value3')
+# original_dict['key2'] = "value4"
+# print(shallow_copied_dict)
+
+word = ["A", "A", "B"]
+count = 0
+b = 0
+i = len(word) - 1
+while i >= 0:
+    if word[i] == 'B':
+        b += 1
+    else:
+        count += b
+        b = 0
+        word[i] = 'B'
+        i += 1
+    
+    i -= 1
+
+print(count)
