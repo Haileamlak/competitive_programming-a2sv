69d29afbadc38a81d77502b56b544a52c2920166
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-17 22:12:02 -0800

    Added solution for Maximum Product Difference Between Two Pairs

--- python_track/maximum-product-difference-between-two-pairs.py
+++ python_track/maximum-product-difference-between-two-pairs.py
@@ -1,5 +1,28 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        nums.sort()
+        # nums.sort()
+
+        min1 = 10**4
+        min2 = min1
+
+        max1 = 0
+        max2 = max1
         
-        return nums[-1] * nums[-2] - nums[0] * nums[1]
+        for num in nums:
+            if num > max1:
+                max2 = max1
+                max1 = num
+
+            elif num > max2:
+                max2 = num
+
+            if num < min1:
+                min2 = min1
+                min1 = num
+            
+            elif num < min2:
+                min2 = num
+
+        return max1 * max2 - min1 * min2
+
+        # return nums[-1] * nums[-2] - nums[0] * nums[1]
