ecdfa631ce7101a7d97bd92154f3d3c4243d3d5d
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-23 06:53:41 -0900

    added solutions to contest 1

---
+++ C_Heavy_Intervals.py
@@ -0,0 +1,5 @@
+t = int(input())
+
+for _ in range(t):
+    left = list(map(int, input().split()))
+    right = list(map(int, input().split()))
--- contest_1/C_Pneumonoultramicroscopicsilicovolcanoconiosis.py
+++ contest_1/C_Pneumonoultramicroscopicsilicovolcanoconiosis.py
@@ -4,6 +4,6 @@ for _ in range(n):
     word = input()
     length = len(word)
     if length > 10:
-        print(word[0] + str(length - 2) + word[length - 1])
+        print(word[0] + str(length - 2) + word[-1])
     else:
         print(word)
