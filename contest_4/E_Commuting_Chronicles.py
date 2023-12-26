dfb10f5eda1a4d8e5aa648a63987ea37556d23b8
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-25 20:43:55 -0900

    upsolved the last problem

--- contest_4/E_Commuting_Chronicles.py
+++ contest_4/E_Commuting_Chronicles.py
@@ -1,70 +1,98 @@
+def transpose(matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        transpose = []
+        for i in range(n):
+            transpose.append([])
+            for j in range(m):
+                transpose[i].append(matrix[j][i])
+        
+        return transpose
+
+
+def isPossible(road, srow, trow, scol, tcol, n, m):
+    for i in range(scol + 1, m):
+        if road[srow][i] == "*":
+            break
+        road[srow][i] = "S"
+
+    for i in range(scol - 1, -1, -1):
+        if road[srow][i] == "*":
+            break
+        road[srow][i] = "S"
+
+
+    for i in range(tcol + 1, m):
+        if road[trow][i] == "*":
+            break
+        road[trow][i] = "T"
+
+    for i in range(tcol - 1, -1, -1):
+        if road[trow][i] == "*":
+            break
+        road[trow][i] = "T"
+    
+    # print(road)
+    if srow == trow:
+        start = min(scol, tcol)
+        end = max(scol, tcol)
+        for col in range(start + 1, end):
+            if road[srow][col] == '*':
+                break
+            
+        else:
+            return True
+
+    elif srow < trow:
+        for col in range(m):
+            if road[srow][col] == "S" and road[trow][col] == "T":
+            
+                for i in range(srow + 1, trow):
+                    if road[i][col] == "*":
+                        break
+                else:
+                    return True
+
+    else:
+        for col in range(m):
+            if road[srow][col] == "S" and road[trow][col] == "T":
+            
+                for i in range(trow + 1, srow):
+                    if road[i][col] == "*":
+                        break
+                else:
+                    return True
+    
+    return False
+
 
 nm = list(map(int, input().split()))
-
 n = nm[0]
 m = nm[1]
 
-road = []
-for i in range(n):
-    row = input()
-    # line = [ch for ch in row]
-    road.append(row)
-
-# print(road
-openrow = [0, m]
-
 srow = 0
 trow = 0
-
+scol = 0
+tcol = 0
+road = []
 for i in range(n):
-    if road[i].count("S"):
-
-        col = road[i].index("S")
+    row = [ch for ch in input()]
+    if row.count('S'):
+        scol = row.index("S")
         srow = i
 
-        while col >= 0 and road[i][col] != '*':
-            col -= 1
-
-        col += 1
-        openrow[0] = max(col, openrow[0])
-
-        while col < m and road[col] != '*':
-            col += 1
-        
-        openrow[1] = min(col - 1, openrow[1])
-    
-    elif road[i].count('T'):
-
-        col = road[i].index("T")
+    if row.count('T'):
+        tcol = row.index("T")
         trow = i
-
-        while col >= 0 and road[i][col] != '*':
-            col -= 1
-
-        col += 1
-        openrow[0] = max(col, openrow[0])
-        while col < m and road[col] != '*':
-            col += 1
         
-        openrow[1] = min(col - 1, openrow[1])
+    road.append(row)
 
+road_transpose = transpose(road)
 
-st = min(srow, trow)
-en = max(srow, trow)
-
-for i in range(openrow[0], openrow[1] + 1):
-    cnt = 0
-    for j in range(st, en + 1):
-        if road[i][j] == '*':
-            cnt -= 1
-
-    if cnt == 0:
-        print("YES")
-        break
+if isPossible(road, srow,trow,scol,tcol, n, m):
+    print("YES")
+elif isPossible(road_transpose,scol,tcol, srow, trow , m, n):
+    print("YES")
 else:
     print("NO")
-
-
-
-
-
