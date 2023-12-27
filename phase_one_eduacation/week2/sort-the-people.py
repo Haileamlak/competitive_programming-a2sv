2076bc512ae0eaeb2c95e0def439ae5ac3c058cb
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-27 05:58:26 -0900

    Added solution for Sort the People

--- phase_one_eduacation/week2/sort-the-people.py
+++ phase_one_eduacation/week2/sort-the-people.py
@@ -1,9 +1,15 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names)):
-            for j in range(len(names) - i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
+        for i in range(1, len(names)):
+            temp_height = heights[i]
+            temp_name = names[i]
+            j = i
+            while j >= 1 and temp_height > heights[j - 1]:
+                heights[j] = heights[j - 1]
+                names[j] = names[j - 1]
+                j -= 1
+
+            heights[j] = temp_height
+            names[j] = temp_name
 
         return names
