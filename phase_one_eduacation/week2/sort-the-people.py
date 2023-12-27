a05fd21d11015d5d2c265085f85e90714369ba1f
Haileamlak Belachew <haileamlakbeat@gmail.com>
2023-12-27 06:58:01 -0900

    Added solution for Sort the People

--- phase_one_eduacation/week2/sort-the-people.py
+++ phase_one_eduacation/week2/sort-the-people.py
@@ -6,12 +6,11 @@ class Solution:
 
         for i in range(len(names)):
             height_count[heights[i]] += 1
-            names_map[heights[i]] = names_map.get(heights[i], []) + [names[i]]
+            names_map[heights[i]] = names[i]
         
         res = []
         for i in range(high, -1, -1):
-            while height_count[i] > 0:
-                res.append(names_map[i][height_count[i] - 1])
-                height_count[i] -= 1
+            if height_count[i]:
+                res.append(names_map[i])
 
         return res
