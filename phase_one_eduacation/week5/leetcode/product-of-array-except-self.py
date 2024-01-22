8a9bdf5499ae704b8019182623185ca6be500e9d
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-01-22 05:13:06 -0900

    Add solution for Product of Array Except Self

--- phase_one_eduacation/week5/leetcode/product-of-array-except-self.py
+++ phase_one_eduacation/week5/leetcode/product-of-array-except-self.py
@@ -4,19 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        left_product = [0 for _ in range(len(nums))]
-        left_product[0] = nums[0]
+        res = [1 for _ in range(len(nums))]
 
-        right_product = [0 for _ in range(len(nums))]
-        right_product[-1] = nums[-1]
         for i in range(1, len(nums)):
-            left_product[i] = left_product[i - 1] * nums[i]
-            right_product[-1*(i + 1)] = right_product[-1*i] * nums[-1*(i + 1)]
+            res[i] = res[i - 1] * nums[i - 1]
         
-        # nums = [0 for _ in range(len(nums))]
-        nums[0] = right_product[1]
-        nums[-1] = left_product[-2]
-        for i in range(1, len(nums) - 1):
-            nums[i] = left_product[i - 1] * right_product[i + 1]
+        right_product = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= right_product
+            right_product *= nums[i]
 
-        return nums
+        return res
