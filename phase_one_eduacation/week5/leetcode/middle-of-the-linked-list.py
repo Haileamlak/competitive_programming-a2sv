f36a87150d2616f83899d8c3b5d0d2a01ee11d1c
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-06 09:58:42 -0900

    Add solution for Middle of the Linked List

---
+++ phase_one_eduacation/week5/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slowPtr = fastPtr = head
+
+        while fastPtr and fastPtr.next:
+            slowPtr = slowPtr.next
+            fastPtr = fastPtr.next.next
+        
+        return slowPtr
