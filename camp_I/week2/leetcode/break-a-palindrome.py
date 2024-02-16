d931e9e1d89b3ab1ba43b5eb406fb89c07f576d7
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-16 09:35:19 -0900

    Add solution for Break a Palindrome

--- camp_I/week2/leetcode/break-a-palindrome.py
+++ camp_I/week2/leetcode/break-a-palindrome.py
@@ -4,20 +4,17 @@ class Solution:
             return ''
         
         index = 0
-        while index < len(palindrome) and palindrome[index] == 'a': 
+        while index < len(palindrome): 
+            if palindrome[index] != 'a' and ((len(palindrome) % 2 == 0 or index != len(palindrome) // 2)):
+                break
+
             index += 1
         
-        if (len(palindrome) % 2 != 0 and index == len(palindrome) // 2):
-            index += 1
-            while index < len(palindrome) and palindrome[index] == 'a': 
-                index += 1
-
         non_palindrome = list(palindrome)
 
         if index == len(palindrome):
             non_palindrome[-1] = 'b'
         else:
-            # if :
             non_palindrome[index] = 'a'
 
         return ''.join(non_palindrome)
