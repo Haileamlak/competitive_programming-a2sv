a30f07ee1870ffa3f9f3e2371adcb97086b25e29
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-20 07:01:46 -0900

    Add solution for Reverse String

--- camp_I/week2/leetcode/reverse-string.py
+++ camp_I/week2/leetcode/reverse-string.py
@@ -1,13 +1,16 @@
 class Solution:
+    def reverseHelper(self, s, left, right):
+        if left >= right:
+            return
+        
+        s[left], s[right] = s[right], s[left]
+
+        self.reverseHelper(s, left + 1, right - 1)
+
+
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        start = 0
-        end = len(s) - 1
-
-        while start < end:
-            s[start], s[end] = s[end], s[start]
-
-            start += 1
-            end -= 1        
+        self.reverseHelper(s, 0, len(s) - 1)
+        
