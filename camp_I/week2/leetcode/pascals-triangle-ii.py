ea11b37d680b25e758a5963e072d99b6ad30e523
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-20 23:12:51 -0900

    Add solution for Pascal's Triangle II

---
+++ camp_I/week2/leetcode/pascals-triangle-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        if rowIndex == 1:
+            return [1, 1]
+
+        rowBefore = self.getRow(rowIndex - 1)
+
+        res = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex):
+            res[i] = rowBefore[i - 1] + rowBefore[i]
+        
+        return res
