a8059814a3f2c3fc556a37e567992b0bc1ea2d20
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-23 09:57:25 -0900

    Add solution for Two City Scheduling

--- camp_I/week3/leetcode/two-city-scheduling.py
+++ camp_I/week3/leetcode/two-city-scheduling.py
@@ -3,7 +3,7 @@ class Solution:
         costs.sort(key=lambda x:(x[0] - x[1]))
 
         res = 0
-        for i in range(len(costs)//2):
+        for i in range(len(costs) // 2):
             res += costs[i][0] + costs[len(costs) - 1 - i][1]
         
         return res
