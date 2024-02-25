db9201ca313abb107fb95e6912020aac35dbcca7
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-25 07:29:21 -0900

    Add solution for Remove Duplicates from Sorted Array

---
+++ camp_I/week3/leetcode/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seeker = 1
+        placeholder = 1
+        while seeker < len(nums):
+            if nums[seeker] != nums[seeker - 1]:
+                nums[placeholder] = nums[seeker]
+                placeholder += 1
+            
+            seeker += 1
+        
+        return placeholder
