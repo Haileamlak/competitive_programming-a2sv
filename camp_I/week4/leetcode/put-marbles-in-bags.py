d6079731ad1e39c7a7501e3f0128fbf23ec2350c
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-27 03:09:12 -0900

    Add solution for Put Marbles in Bags

--- camp_I/week4/leetcode/put-marbles-in-bags.py
+++ camp_I/week4/leetcode/put-marbles-in-bags.py
@@ -1,14 +1,13 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        splitting = [0] * (len(weights) - 1)
+        pair_weights = [0] * (len(weights) - 1)
         for i in range(len(weights) - 1):
-            splitting[i] = weights[i] + weights[i + 1]
-            
-        splitting.sort()
+            pair_weights[i] = weights[i] + weights[i + 1]
 
-        min_score = max_score = weights[0] + weights[-1]
+        pair_weights.sort()
+
+        answer = 0
         for i in range(k - 1):
-            min_score += splitting[i]
-            max_score += splitting[-1 - i]
+            answer += pair_weights[-1 - i] - pair_weights[i]
         
-        return max_score - min_score
+        return answer
