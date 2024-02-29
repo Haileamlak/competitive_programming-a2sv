8b9c11555b7906ada024e88282f5f369aebe610b
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-29 03:53:49 -0900

    Add solution for Combination Sum II

---
+++ camp_I/week4/leetcode/combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        combinations = []
+        combination = []
+        candidates.sort()
+
+        def back_track(index, curr_sum):
+            if curr_sum == target:
+                combinations.append(combination.copy())
+                return
+            
+            if index == len(candidates):
+                return
+
+            next_index = index + 1
+            while next_index < len(candidates) and candidates[next_index] == candidates[index]:
+                next_index += 1
+
+            combination.append(candidates[index])
+            if curr_sum + candidates[index]<= target:
+                back_track(index + 1, curr_sum + candidates[index])
+            combination.pop()
+            if curr_sum <= target:
+                back_track(next_index, curr_sum)
+
+        back_track(0, 0)
+        return combinations
