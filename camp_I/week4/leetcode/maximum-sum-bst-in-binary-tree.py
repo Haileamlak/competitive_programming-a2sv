b2e3c59321002c847a2a035bfaebf917bc794662
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-28 21:38:45 -0900

    Add solution for Maximum Sum BST in Binary Tree

--- camp_I/week4/leetcode/maximum-sum-bst-in-binary-tree.py
+++ camp_I/week4/leetcode/maximum-sum-bst-in-binary-tree.py
@@ -7,47 +7,44 @@
 class Solution:
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
         res = 0
-
         def traverse(root):
             nonlocal res
+            
             if not root.left and not root.right:
                 res = max(root.val, res)
                 return (True, root.val, root.val, root.val) # (isBST, min, max, sum)
-            
+
             if not root.left:
                 right = traverse(root.right)
+                curr_sum = root.val + right[3]
+                isBst = right[0] and (root.val < right[1])
 
-                curr = root.val + right[3]
+                if isBst:
+                    res = max(res, curr_sum)
 
-                if right[0] and (root.val < right[1]):
-                    res = max(res, curr)
-                    return (True, min( right[1], root.val), max( right[2], root.val), curr)
-            
-                return (False, min( right[1], root.val), max( right[2], root.val), curr)
+                return (isBst, min( right[1], root.val), max( right[2], root.val), curr_sum)
             
             if not root.right:
                 left = traverse(root.left)
-
-                curr = root.val + left[3]
-
-                if left[0] and (root.val > left[2]):
-                    res = max(res, curr)
-                    return (True, min( left[1], root.val), max( left[2], root.val), curr)
-            
-                return (False, min( left[1], root.val), max( left[2], root.val), curr)
+                curr_sum = root.val + left[3]
+                isBst =  left[0] and (root.val > left[2])
                 
+                if isBst:
+                    res = max(res, curr_sum)
+            
+                return (isBst, min( left[1], root.val), max( left[2], root.val), curr_sum)
 
-
-
+            
             left = traverse(root.left)
             right = traverse(root.right)
-
-            curr = root.val + left[3] + right[3]
-            if left[0] and right[0] and (left[2] < root.val < right[1]):
-                res = max(res, curr)
-                return (True, min([left[1], right[1], root.val]), max([left[2], right[2], root.val]), curr)
             
-            return (False, min([left[1], right[1], root.val]), max([left[2], right[2], root.val]), curr)
+            curr_sum = root.val + left[3] + right[3]
+            isBst = left[0] and right[0] and (left[2] < root.val < right[1])
+
+            if isBst:
+                res = max(res, curr_sum)
+            
+            return (isBst, min([left[1], right[1], root.val]), max([left[2], right[2], root.val]), curr_sum)
 
         traverse(root)
 
