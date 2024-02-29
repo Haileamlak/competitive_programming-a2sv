98ea6cfba7ec7ee855ae571a34f6f45a35bd7b1c
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-02-29 02:53:42 -0900

    Add solution for Word Search

---
+++ camp_I/week4/leetcode/word-search.py
@@ -0,0 +1,31 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        res = False
+        visited = set()
+
+        def back_track(i, j, index):
+            nonlocal res
+            if index == len(word):
+                res = True
+                return
+
+            if ((i, j) in visited) or (i < 0 or i >= m) or (j < 0 or j >= n):
+                return
+
+            if board[i][j] != word[index]:
+                return
+            
+            visited.add((i, j))
+            back_track(i + 1, j, index + 1)
+            back_track(i, j + 1, index + 1)
+            back_track(i - 1, j, index + 1)
+            back_track(i, j - 1, index + 1)
+            visited.remove((i, j))
+        
+        for i in range(m):
+            for j in range(n):
+                back_track(i, j, 0)
+
+        return res
