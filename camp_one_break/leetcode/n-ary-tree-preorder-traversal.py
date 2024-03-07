e93904716fbbb9fb71bf67c7ba5469391e3fd51b
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-06 23:21:52 -0900

    Add solution for N-ary Tree Preorder Traversal

--- camp_one_break/leetcode/n-ary-tree-preorder-traversal.py
+++ camp_one_break/leetcode/n-ary-tree-preorder-traversal.py
@@ -8,11 +8,29 @@ class Node:
 
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
-        if not root:
-            return
+        res = []
+        stack = []
+        curr = root
 
-        res = [root.val]
-        for child in root.children:
-            res += self.preorder(child)
+        while curr or stack:
+            while(curr):
+                res.append(curr.val)
+                queue = deque()
+                for i in range(1, len(curr.children)):
+                    queue.append(curr.children[i])
+                
+                stack.append(queue)
 
+                if curr.children:
+                    curr = curr.children[0]
+                else:
+                    stack.pop()
+                    curr = None
+            
+            if stack and stack[-1]:
+                curr = stack[-1].popleft()
+            elif stack:
+                stack.pop()
+                curr = None
+        
         return res
