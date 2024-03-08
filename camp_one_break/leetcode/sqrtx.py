b9f6d6492e885dbfa5603f0e6b6dd778f0481505
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-08 02:25:10 -0900

    Add solution for Sqrt(x)

--- camp_one_break/leetcode/sqrtx.py
+++ camp_one_break/leetcode/sqrtx.py
@@ -1,7 +1,9 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+
         left, right = 0, x
-        
         while int(left) < int(right):
             mid = left + (right - left) / 2
             if mid < x / mid:
@@ -10,5 +12,5 @@ class Solution:
                 right = mid
             else:
                 return int(mid)
-        
+
         return int(left)
