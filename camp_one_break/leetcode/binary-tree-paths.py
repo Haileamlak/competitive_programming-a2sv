0f017a8e230aa763f5ac86b7412b7a53d673a205
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-08 03:58:34 -0900

    Add solution for Binary Tree Paths

--- camp_one_break/leetcode/binary-tree-paths.py
+++ camp_one_break/leetcode/binary-tree-paths.py
@@ -8,18 +8,19 @@ class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
         res = []
         path = []
+
         def depth_first_traverse(root):
-            if not root:
-                return
-                
-            if not root.left and not root.right:
-                path.append(str(root.val))
-                res.append(''.join(path))
-                path.pop()
             
-            path.append(str(root.val) + '->')
-            depth_first_traverse(root.left)
-            depth_first_traverse(root.right)
+            path.append(str(root.val))
+
+            if not root.left and not root.right:
+                res.append('->'.join(path))
+
+            if root.left:
+                depth_first_traverse(root.left)
+            if root.right:
+                depth_first_traverse(root.right)
+
             path.pop()
         
         depth_first_traverse(root)
