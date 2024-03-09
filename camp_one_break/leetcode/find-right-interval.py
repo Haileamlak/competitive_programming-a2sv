67ffb26427d9d16705b94c7d6894640cbe0f5663
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-09 03:09:41 -0900

    Add solution for Find Right Interval

--- camp_one_break/leetcode/find-right-interval.py
+++ camp_one_break/leetcode/find-right-interval.py
@@ -1,25 +1,18 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
         res = [-1] * len(intervals)
-
+        index_map = {intervals[i][0]:i for i in range(len(intervals))}
         
-        temp1 = [(intervals[i], i) for i in range(len(intervals))]
-        temp1.sort(key = lambda x : x[0][0])
+        start_index = [interval[0] for interval in intervals]
+        start_index.sort()
         
         for i in range(len(res)):
             left, right = 0, len(intervals) - 1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if temp1[mid][0][0] < intervals[i][1]:
-                    left = mid + 1
-                
-                elif temp1[mid][0][0] > intervals[i][1]:
-                    right = mid - 1
-                else:
-                    left = mid
-                    break
+
+            end = intervals[i][1]
+            index = bisect_left(start_index, end)
             
-            if left < len(intervals):
-                res[i] = temp1[left][1]
+            if index < len(intervals):
+                res[i] = index_map[start_index[index]]
         
         return res
