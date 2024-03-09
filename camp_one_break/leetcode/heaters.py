7f014d92176bfd00b12b8dd04d6dd6166cb1c000
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-09 02:08:49 -0900

    Add solution for Heaters

--- camp_one_break/leetcode/heaters.py
+++ camp_one_break/leetcode/heaters.py
@@ -2,22 +2,16 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         min_radius = 0
         heaters.sort()
+        houses.sort()
+        heaters.append(float('inf'))
+        heaters.append(float('-inf'))
 
+        heat_ptr = 0
         for house in houses:
-            left, right = 0, len(heaters) - 1
-            radius = float('inf')
-            while left <= right:
-                mid = left + (right - left) // 2
-
-                radius = min(radius, abs(house - heaters[mid]))
-
-                if heaters[mid] < house:
-                    left = mid + 1
-                elif heaters[mid] > house:
-                    right = mid - 1
-                else:
-                    break
+            while heaters[heat_ptr] < house:
+                heat_ptr += 1
             
+            radius = min(abs(house - heaters[heat_ptr]), abs(house - heaters[heat_ptr - 1]))
             min_radius = max(min_radius, radius)
             
         return min_radius
