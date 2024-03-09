31033fdad6bca807bf3192d487ddb2778be5ee79
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-09 02:58:11 -0900

    Add solution for Find Right Interval

--- camp_one_break/leetcode/find-right-interval.py
+++ camp_one_break/leetcode/find-right-interval.py
@@ -4,19 +4,22 @@ class Solution:
 
         
         temp1 = [(intervals[i], i) for i in range(len(intervals))]
-        temp1.sort(key = lambda x : x[0][1])
-
-        temp2 = [(intervals[i], i) for i in range(len(intervals))]
-        temp2.sort(key = lambda x : x[0][0])
+        temp1.sort(key = lambda x : x[0][0])
         
-        ptr2 = 0
-        for ptr1 in range(len(temp1)):
-            while ptr2 < len(intervals) and temp1[ptr1][0][1] > temp2[ptr2][0][0]:
-                ptr2 += 1
+        for i in range(len(res)):
+            left, right = 0, len(intervals) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if temp1[mid][0][0] < intervals[i][1]:
+                    left = mid + 1
+                
+                elif temp1[mid][0][0] > intervals[i][1]:
+                    right = mid - 1
+                else:
+                    left = mid
+                    break
             
-            if ptr2 < len(temp2):
-                res[temp1[ptr1][1]] = temp2[ptr2][1]
-            else:
-                break
-
+            if left < len(intervals):
+                res[i] = temp1[left][1]
+        
         return res
