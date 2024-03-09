6e67e3e14a08f43a6992225d0a640f66cde8b2dc
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-09 07:38:09 -0900

    Add solution for H-Index

--- camp_one_break/leetcode/h-index.py
+++ camp_one_break/leetcode/h-index.py
@@ -1,20 +1,9 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse = True)
-        def can_be(h):
-            i = 0
-            while i < len(citations) and citations[i] >= h:
-                i += 1
-            
-            return h <= i
+
+        h = 1
+        while h <= len(citations) and citations[h - 1] >= h:
+            h += 1
         
-        minimum = 0
-        maximum = len(citations)
-        while minimum <= maximum:
-            mid = minimum + (maximum - minimum) // 2
-            if can_be(mid):
-                minimum = mid + 1
-            else:
-                maximum = mid - 1
-        
-        return maximum
+        return h - 1
