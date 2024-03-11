e9260b074d51b11869195a7361515364b1b588b7
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-11 02:01:36 -0800

    Add solution for Maximize Happiness of Selected Children

---
+++ camp_one_break/leetcode/maximize-happiness-of-selected-children.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        res = 0
+        for i in range(k):
+            if happiness[i] > i:
+                res += happiness[i] - i
+        
+        return res
