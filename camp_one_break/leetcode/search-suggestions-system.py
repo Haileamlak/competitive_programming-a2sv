bb2118ac7dd0a2e0f82cdf42ec7183cbccc19a18
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-12 11:49:11 -0800

    Add solution for Search Suggestions System

--- camp_one_break/leetcode/search-suggestions-system.py
+++ camp_one_break/leetcode/search-suggestions-system.py
@@ -1,31 +1,11 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
-        
-        def get_index(left, prefix):
-            right = len(products) - 1
-            while left <= right:
-                mid = left + (right - left) // 2
-
-                x = False
-                if len(products[mid]) <= len(prefix):
-                    x = prefix <= products[mid]
-                else:
-                    x = prefix <= products[mid][:len(prefix)]
-                
-                if x:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            
-            return left
-
-
         res = []
         left = 0
         for i in range(1, len(searchWord) + 1):
             curr = searchWord[:i]
-            left = get_index(left, curr)
+            left = bisect_left(products, curr, left)
 
             temp = []
             for i in range(left, min(len(products), left + 3)):
