f853bf51e0aef45fa5ef982d88defa5c0339c844
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-03-17 19:02:27 +0300

    Added solution to Minimum Subsequence in Non-Increasing Order

---
+++ camp_one_break/LeetCode/Minimum Subsequence in Non-Increasing Order.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse = True)
+        total = sum(nums)
+        curr = 0
+        res = []
+
+        i = 0
+        while curr <= total:
+            curr += nums[i]
+            total -= nums[i]
+            res.append(nums[i])
+            i += 1
+        
+        return res
