026a1f3bbc68f68b3985a2754f1f5dcbd7286fb4
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-09 08:31:53 +0300

    Created 'A - Flag Checker' by A2SV Hub

---
+++ A2SV G5 - Contest #11 09-Apr-2024/A - Flag Checker 118916.py
@@ -0,0 +1,27 @@
+# Problem: A - Flag Checker - https://codeforces.com/gym/515998/problem/A
+
+from typing import Counter
+
+
+n, m = map(int, input().split())
+res = []
+for i in range(n):
+    row = (input())
+    res.append((Counter(row), row[0]))
+
+if len(res[0][0]) != 1:
+    print('NO')
+    
+else:
+    for i in range(1,n):
+
+        if len((res[i][0])) > 1:
+            print('NO')
+            break
+            
+        if res[i][1] == res[i - 1][1]:
+            print("NO")
+            break
+    else:
+        print('YES')
+    
