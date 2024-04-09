aec7f37cc7e4733efb0f95e8d5b05193f212e4a4
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-09 08:31:48 +0300

    Created 'B - Mapping the Maze' by A2SV Hub

---
+++ A2SV G5 - Contest #11 09-Apr-2024/B - Mapping the Maze 118915.py
@@ -0,0 +1,28 @@
+# Problem: B - Mapping the Maze - https://codeforces.com/gym/515998/problem/B
+
+from collections import defaultdict
+
+
+n, m = map(int, input().split())
+graph = defaultdict(list)
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+count = defaultdict(int)
+
+for v in graph:
+    count[len(graph[v])] += 1
+
+if count[n - 1] == 1 and count[1] == n - 1:
+    
+    print("star topology")
+elif count[2] == n - 2 and count[1] == 2:
+    print('bus topology')
+
+elif count[2] == n:
+    print("ring topology")
+else:
+    print('unknown topology')    
