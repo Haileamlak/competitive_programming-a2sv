b2d2531d77825f4af45f94734be2c49d31dd083d
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-09 09:09:53 +0300

    Created 'F -  A Permutation Puzzle' by A2SV Hub

---
+++ A2SV G5 - Contest #11 09-Apr-2024/F -  A Permutation Puzzle 118937.py
@@ -0,0 +1,28 @@
+# Problem: F -  A Permutation Puzzle - https://codeforces.com/gym/515998/problem/F
+
+from math import gcd
+
+
+for i in range(int(input())):
+    n = int(input())
+    word = input()    
+    per = list(map(int, input().split()))
+    
+    def shift(cycle):
+        temp = cycle + cycle
+        return temp[1:].find(cycle) + 1
+    
+    res = 1
+    visited = [False] * n
+    for i in range(n):
+        if not visited[i]:
+            cycle = ''
+            while not visited[i]:
+                visited[i] = True
+                cycle += word[i]
+                i = per[i] - 1
+            
+            x = shift(cycle)
+            res = (res * x) // (gcd(res, x))
+
+    print(res)
