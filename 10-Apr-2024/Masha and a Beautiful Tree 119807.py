90f32c469e160c58d27e5386e6c8b159d7614807
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-10 14:50:52 +0300

    Created 'Masha and a Beautiful Tree' by A2SV Hub

---
+++ 10-Apr-2024/Masha and a Beautiful Tree 119807.py
@@ -0,0 +1,30 @@
+# Problem: Masha and a Beautiful Tree - https://codeforces.com/problemset/problem/1741/D
+
+def make_beautiful(nums, left, right):
+    if left == right:
+        return (True, nums[left], nums[right], 0)
+    
+    mid = (left + right) // 2
+    left = make_beautiful(nums, left, mid )
+    right = make_beautiful(nums, mid + 1, right)
+
+    res = -1
+    if left[2] < right[1]:
+        res = 0
+    elif right[2] < left[1]:
+        res = 1
+
+    isBeautiful = left[0] and right[0] and (res > -1)
+
+    return (isBeautiful, min(left[1], right[1]), max(left[2], right[2]), res + left[3] + right[3])
+
+
+for _ in range(int(input())):
+    m = int(input())
+    nums = list(map(int, input().split()))
+
+    res = make_beautiful(nums, 0, m - 1)
+    if not res[0]:
+        print(-1)
+    else:
+        print(res[3])
