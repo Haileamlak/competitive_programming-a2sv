c19be92e4690f527193bd871fa25e9aad21d97b0
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-15 09:18:34 +0300

    Created 'They Are Everywhere' by A2SV Hub

---
+++ 15-Apr-2024/They Are Everywhere 123385.py
@@ -0,0 +1,28 @@
+# Problem: They Are Everywhere - https://codeforces.com/problemset/problem/701/C
+
+from collections import defaultdict
+
+
+n = int(input())
+word = input()
+
+unique = set(word)
+window = defaultdict(int)
+
+res = n
+left = 0
+for right in range(n):
+    window[word[right]] += 1
+    
+    while len(window) >= len(unique):
+        window[word[left]] -= 1
+
+        if window[word[left]] == 0:
+            del window[word[left]]
+        
+        left += 1
+    
+    if left > 0:
+        res = min(res, right - left + 2)
+
+print(res)
