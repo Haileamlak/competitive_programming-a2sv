2c65a311157e4eb23e37f310c446b79e0676302b
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-17 17:27:24 +0300

    Created 'Remove Stones to Minimize the Total' by A2SV Hub

---
+++ 17-Apr-2024/Remove Stones to Minimize the Total 125626.py
@@ -0,0 +1,14 @@
+# Problem: Remove Stones to Minimize the Total - https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        temp = [-pile for pile in piles]
+        heapify(temp)
+        while k:
+            x = -heappop(temp)
+            x = ceil(x / 2)
+            heappush(temp, -x)
+
+            k -= 1
+        
+        return -sum(temp)
