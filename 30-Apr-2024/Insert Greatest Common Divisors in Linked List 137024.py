6c9f166a93961288c895658623a234fa1fa3bdca
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-04-30 16:14:46 +0300

    Created 'Insert Greatest Common Divisors in Linked List' by A2SV Hub

---
+++ 30-Apr-2024/Insert Greatest Common Divisors in Linked List 137024.py
@@ -0,0 +1,17 @@
+# Problem: Insert Greatest Common Divisors in Linked List - https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        curr = head
+        
+        while curr.next:
+            curr.next = ListNode(gcd(curr.val, curr.next.val), curr.next)
+            curr = curr.next.next
+        
+        return head
