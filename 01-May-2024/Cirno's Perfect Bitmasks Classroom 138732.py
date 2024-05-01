fc8b144c23a5d5a58eb8940f83782ef71edab61f
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-01 22:07:28 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 138732.py
@@ -0,0 +1,12 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+for i in range(int(input())):
+    x = int(input())
+
+    if x == 1:
+        print(3)
+        
+    elif x & (x - 1) == 0:
+        print(x + 1)
+    else:
+        print(x - (x & (x - 1)))
