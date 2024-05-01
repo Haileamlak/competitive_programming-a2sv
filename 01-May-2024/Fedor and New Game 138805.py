c294bd93c67bfd52416ae197ca42d7d7308fd94d
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-01 23:11:19 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 01-May-2024/Fedor and New Game 138805.py
@@ -0,0 +1,23 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+players = []
+
+for i in range(m + 1):
+    players.append(int(input()))
+
+def number_of_different(num1, num2):
+    temp = num1 ^ num2
+    count = 0
+    while temp:
+        temp &= temp - 1
+        count += 1
+    
+    return count
+
+res = 0
+for i in range(m):
+    if number_of_different(players[i], players[m]) <= k:
+        res += 1
+
+print(res)
