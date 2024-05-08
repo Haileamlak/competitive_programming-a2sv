015a1a9a2b21f7e9898aa5db40317953cb275944
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-08 22:28:10 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 08-May-2024/B - tourist orz! 143610.py
@@ -0,0 +1,10 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+for i in range(int(input())):
+    n, z = map(int, input().split())
+    a = list(map(int, input().split()))
+    res = 0
+    for num in a:
+        res = max(res, num | z)
+
+    print(res)
