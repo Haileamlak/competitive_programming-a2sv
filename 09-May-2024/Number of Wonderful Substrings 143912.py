673209b5b8a513ac40350f2ddec4b958ef44ad6f
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-09 10:21:46 +0300

    Created 'Number of Wonderful Substrings' by A2SV Hub

---
+++ 09-May-2024/Number of Wonderful Substrings 143912.py
@@ -0,0 +1,21 @@
+# Problem: Number of Wonderful Substrings - https://leetcode.com/problems/number-of-wonderful-substrings/
+
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        count = [0] * 1024
+        count[0] = 1
+
+        res = 0
+        mask = 0
+        for letter in word:
+            pos = ord(letter) - ord('a')
+            mask ^= (1 << pos)
+
+            res += count[mask]
+
+            for i in range(10):
+                res += count[mask ^ (1 << i)]
+
+            count[mask] += 1
+        
+        return res
