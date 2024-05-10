093f8c08010bc1219e045c430d859c3effa788b3
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-10 09:22:36 +0300

    Created 'Maximize Happiness of Selected Children' by A2SV Hub

---
+++ 10-May-2024/Maximize Happiness of Selected Children 145164.py
@@ -0,0 +1,11 @@
+# Problem: Maximize Happiness of Selected Children - https://leetcode.com/problems/maximize-happiness-of-selected-children/?envType=daily-question&envId=2024-05-09
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        res = 0
+        for i in range(k):
+            if happiness[i] > i:
+                res += happiness[i] - i
+        
+        return res
