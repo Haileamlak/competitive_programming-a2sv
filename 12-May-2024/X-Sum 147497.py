6ab28cdeca3d6cc37853e0e7f1fa29ac172597f6
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-12 18:49:30 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 12-May-2024/X-Sum 147497.py
@@ -0,0 +1,25 @@
+# Problem: X-Sum - https://codeforces.com/contest/1676/problem/D
+
+from collections import defaultdict
+
+for i in range(int(input())):
+    n, m = map(int, input().split())
+    chess = []
+    for i in range(n):
+        row = list(map(int, input().split()))
+        chess.append(row)
+
+    count1 = defaultdict(int)
+    count2 = defaultdict(int)
+
+    for row in range(n):
+        for col in range(m):
+            count1[row - col] += chess[row][col]
+            count2[row + col] += chess[row][col]
+    
+    res = 0
+    for row in range(n):
+        for col in range(m):
+            res = max(res, count1[row - col] + count2[row + col] - chess[row][col])
+    
+    print(res)
