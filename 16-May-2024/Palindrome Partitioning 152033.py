483db2a5759872dc90d0fbbf80f591da69b12dde
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-16 15:44:34 +0300

    Created 'Palindrome Partitioning' by A2SV Hub

---
+++ 16-May-2024/Palindrome Partitioning 152033.py
@@ -0,0 +1,31 @@
+# Problem: Palindrome Partitioning - https://leetcode.com/problems/palindrome-partitioning/
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        def is_palindrome(string):
+            for i in range(len(string) // 2):
+                if string[i] != string[-1 - i]:
+                    return False
+
+            return True
+
+        @cache
+        def _partition(i):
+            if i == len(s) - 1:
+                return [[s[i]]]
+
+            res = []
+            for j in range(i + 1, len(s)):
+                curr = s[i:j]
+                if is_palindrome(curr):
+                    temp = _partition(j)
+                    for part in temp:
+                        res.append([curr] + part)
+            
+            if is_palindrome(s[i:]):
+                res.append([s[i:]])
+                
+            return res
+        
+        return _partition(0)
