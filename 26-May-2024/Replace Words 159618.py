c110095571f84ae98863c94225df51248cb434a0
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-05-26 09:57:53 +0300

    Created 'Replace Words' by A2SV Hub

---
+++ 26-May-2024/Replace Words 159618.py
@@ -0,0 +1,49 @@
+# Problem: Replace Words - https://leetcode.com/problems/replace-words/
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.is_end = False
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for letter in word:
+            i = ord(letter) - ord('a')
+            if curr.children[i] == None:
+                curr.children[i] = TrieNode()
+            
+            curr = curr.children[i]
+
+        curr.is_end = True
+
+    def find_root(self, word):
+        curr = self.root
+        res = []
+        for index, letter in enumerate(word):
+            i = ord(letter) - ord('a')
+            if not curr.children[i]:
+                return len(word)
+            
+            curr = curr.children[i]
+
+            if curr.is_end:
+                return index + 1
+        
+        return len(word)
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie_dictionary = Trie()
+        for word in dictionary:
+            trie_dictionary.insert(word)
+        
+        res = []
+        for successor in sentence.split(' '):
+            index = trie_dictionary.find_root(successor)
+            res.append(successor[:index])
+        
+        return ' '.join(res)
