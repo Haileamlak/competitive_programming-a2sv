8f21d8609366c8bb783c04f09dbd0edd237df5c2
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-06-02 20:08:23 +0300

    Created 'Count the Number of Fair Pairs' by A2SV Hub

---
+++ 02-Jun-2024/Count the Number of Fair Pairs 163338.py
@@ -0,0 +1,35 @@
+# Problem: Count the Number of Fair Pairs - https://leetcode.com/problems/count-the-number-of-fair-pairs/description/
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+
+        def binary_search_left(i, number):
+            left, right = i, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] + number >= lower:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            return left
+        
+        def binary_search_right(i, number):
+            left, right = i, len(nums) - 1
+            while left <= right:
+                mid = left + (right - left) // 2
+                if nums[mid] + number <= upper:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return right
+        
+        res = 0
+        nums.sort()
+        for i in range(len(nums)):
+            right = binary_search_right(i + 1, nums[i])
+            left = binary_search_left(i + 1, nums[i])
+            res += max(0, right - left + 1)
+        
+        return res
