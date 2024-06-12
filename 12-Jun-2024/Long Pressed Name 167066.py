c4c057071e4681dc8b4016b83d8da07f8b1b4ee5
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-06-12 06:36:32 -0800

    Created 'Long Pressed Name' by A2SV Hub

---
+++ 12-Jun-2024/Long Pressed Name 167066.py
@@ -0,0 +1,34 @@
+# Problem: Long Pressed Name - https://leetcode.com/problems/long-pressed-name/
+
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        """
+        :type name: str
+        :type typed: str
+        :rtype: bool
+        """
+        i = j = 0
+        while i < len(name) and j < len(typed):
+            if name[i] != typed[j]:
+                return False
+
+            i += 1
+            j += 1
+
+            top = 0
+            while i < len(name) and name[i] == name[i - 1]: 
+                i += 1
+                top += 1
+
+            bottom = 0
+            while j < len(typed) and typed[j] == typed[j - 1]:
+                j += 1
+                bottom += 1
+            
+            if bottom < top:
+                return False
+
+        if i < len(name) or j < len(typed):
+            return False
+            
+        return True
