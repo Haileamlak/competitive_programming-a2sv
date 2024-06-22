4f02bd5b702ad189602324c39467b80bf2ce179f
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-06-22 10:51:40 +0300

    Created 'Count Number of Nice Subarrays' by A2SV Hub

---
+++ 22-Jun-2024/Count Number of Nice Subarrays 171403.py
@@ -0,0 +1,18 @@
+# Problem: Count Number of Nice Subarrays - https://leetcode.com/problems/count-number-of-nice-subarrays/
+
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        def at_most(k):
+            res = left = curr = 0
+            for right in range(len(nums)):
+                curr += nums[right] % 2
+                
+                while curr > k:
+                    curr -= nums[left] % 2
+                    left += 1
+                
+                res += right - left + 1
+            
+            return res
+        
+        return at_most(k) - at_most(k - 1)
