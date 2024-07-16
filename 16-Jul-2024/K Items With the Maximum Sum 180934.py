75693901dab930788b856715d784018a7ba5f787
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-16 21:36:22 +0300

    Created 'K Items With the Maximum Sum' by A2SV Hub

---
+++ 16-Jul-2024/K Items With the Maximum Sum 180934.py
@@ -0,0 +1,19 @@
+# Problem: K Items With the Maximum Sum - https://leetcode.com/problems/k-items-with-the-maximum-sum/
+
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        res = 0
+        if numOnes >= k:
+            return k
+
+        res += numOnes
+        k -= numOnes
+
+        if numZeros >= k:
+            return res
+
+        k -= numZeros
+
+        res -= min(numNegOnes, k)
+        
+        return res
