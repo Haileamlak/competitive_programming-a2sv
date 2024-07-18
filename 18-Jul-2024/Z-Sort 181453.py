eb52dbfa8a29927c425b8e67dc3d5e118c6056cd
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-18 10:52:37 +0300

    Created 'Z-Sort' by A2SV Hub

---
+++ 18-Jul-2024/Z-Sort 181453.py
@@ -0,0 +1,11 @@
+# Problem: Z-Sort - https://codeforces.com/problemset/problem/652/B
+
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort()
+for i in range(n // 2):
+    print(a[i], a[n - i - 1], end=' ')
+
+if n % 2:
+    print(a[n // 2])
