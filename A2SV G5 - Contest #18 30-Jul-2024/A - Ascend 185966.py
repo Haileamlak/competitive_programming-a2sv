486b395de21233c26f266958b610b073ca525693
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:29:17 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 30-Jul-2024/A - Ascend 185966.py
@@ -0,0 +1,14 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n = int(input())
+a = list(map(int, input().split()))
+res = 1
+count = 1
+for i in range(1, n):
+    if a[i] > a[i - 1]:
+        count += 1
+    else:
+        count = 1
+    res = max(res, count)
+
+print(res)
