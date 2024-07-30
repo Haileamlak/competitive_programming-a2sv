0c5c6534416f06a09d3446253f8dee2a29edd133
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:29:50 +0300

    Created 'C - Coffee Dilemma' by A2SV Hub

---
+++ A2SV G5 - Contest #20 30-Jul-2024/C - Coffee Dilemma 185968.py
@@ -0,0 +1,24 @@
+# Problem: C - Coffee Dilemma - https://codeforces.com/gym/528792/problem/C
+
+from heapq import heappop, heappush
+
+n = int(input())
+a = list(map(int, input().split()))
+
+curr = 0
+heap = []
+
+res = 0
+for num in a:
+    if num >= 0:
+        curr += num
+        res += 1
+    else:
+        curr += num
+        heappush(heap, num)
+        if curr < 0:
+            curr -= heappop(heap)
+        else:
+            res += 1
+
+print(res)
