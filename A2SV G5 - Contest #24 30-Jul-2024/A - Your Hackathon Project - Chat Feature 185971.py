cafc2aa1d5bc3324fdc4118dca5e534537c19bde
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:31:35 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 30-Jul-2024/A - Your Hackathon Project - Chat Feature 185971.py
@@ -0,0 +1,13 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+for i in range(int(input())):
+    n = int(input())
+    w = input()
+    j = n - 1
+    while j >= 0 and w[j] == ')':
+        j -= 1
+    
+    if n - j - 1  > n // 2:
+        print('YES')
+    else:
+        print('NO')
