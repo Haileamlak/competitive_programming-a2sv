9a1222b3b120894cbf89159c4793ffb10aeb3c6b
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:22:50 +0300

    Created 'F - Post-war Games' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 30-Jul-2024/F - Post-war Games 185956.py
@@ -0,0 +1,10 @@
+# Problem: F - Post-war Games - https://codeforces.com/gym/513152/problem/F
+
+for i  in range(int(input())):
+    n, k = map(int, input().split())
+    if k == n:
+        print(0)
+    else:
+        x = (n - (k + 1)) * 3 + 1 + ((k - 1) // 2) * 3 + ((k - 1) % 2) * 1
+        print(x)
+    
