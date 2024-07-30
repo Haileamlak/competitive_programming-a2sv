e07fde88a539cdcccbb74fed46c5ccb0649f23b4
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:33:03 +0300

    Created 'C - Selecting Theatre Troupe' by A2SV Hub

---
+++ A2SV G5 - Contest #25 30-Jul-2024/C - Selecting Theatre Troupe 185981.py
@@ -0,0 +1,13 @@
+# Problem: C - Selecting Theatre Troupe - https://codeforces.com/gym/535749/problem/C
+
+import sys
+
+from math import comb
+
+n, m, t = map(int, sys.stdin.readline().strip().split())
+
+ans = 0
+for b in range(4, t):
+    ans += comb(n, b)  * comb(m, t  - b)
+
+print(ans)
