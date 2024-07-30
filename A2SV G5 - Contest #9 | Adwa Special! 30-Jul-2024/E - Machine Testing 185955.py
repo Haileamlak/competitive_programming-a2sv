34711006a11f7a200bfbafcd87515bc99c14e7cd
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:22:41 +0300

    Created 'E - Machine Testing' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 30-Jul-2024/E - Machine Testing 185955.py
@@ -0,0 +1,31 @@
+# Problem: E - Machine Testing - https://codeforces.com/gym/513152/problem/E
+
+from math import ceil
+for i  in range(int(input())):
+    n = int(input())
+    h = list(map(int, input().split()))
+    p = list(map(int, input().split()))
+
+    stack = []
+    res = 0
+
+    for i in range(1, n):
+        curr = 0
+        while stack and h[i] > 0:
+            if h[i] >= (stack[-1][0] * stack[-1][1]):
+                h[i] -= (stack[-1][0] * stack[-1][1])
+                curr += stack[-1][1]
+                stack.pop()
+            else:
+                stack[-1][1] -= ceil(h[i] / stack[-1][0])
+                curr += ceil(h[i] / stack[-1][0])
+                h[i] = 0
+
+        if h[i] > 0:
+            x = ceil(h[i] / p[0]) 
+            res += x
+            curr += x
+
+        stack.append([p[i], curr])
+    
+    print(res)
