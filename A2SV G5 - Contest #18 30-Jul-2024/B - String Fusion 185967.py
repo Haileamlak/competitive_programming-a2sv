0a98f9ea4f2a987e5c6a90de1f93c6d92d102ba5
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:29:29 +0300

    Created 'B - String Fusion' by A2SV Hub

---
+++ A2SV G5 - Contest #18 30-Jul-2024/B - String Fusion 185967.py
@@ -0,0 +1,54 @@
+# Problem: B - String Fusion - https://codeforces.com/gym/526229/problem/B
+
+
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = [None] * 26
+        self.count = 0
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for ch in word:
+            i = ord(ch) - 97
+            if not curr.children[i]:
+                curr.children[i] = TrieNode()
+            
+            curr = curr.children[i]
+            curr.count += 1
+        
+    def update(self, word):
+        res = 0
+        curr = self.root
+        for j in range(len(word) - 1, -1, -1):
+            ch = word[j]
+            i = ord(ch) - 97
+            if not curr.children[i]:
+                break
+
+            curr = curr.children[i]
+            res += (2 * curr.count)
+            # print(ch)
+        return res
+
+
+n = int(input())
+words = []
+for _ in range(n):
+    words.append(input())
+
+trie = Trie()
+length = 0
+for word in words:
+    trie.insert(word)
+    length += len(word)
+
+res = length * n
+for word in words:
+    res += n * len(word)
+    res -= trie.update(word)
+
+print(res)
