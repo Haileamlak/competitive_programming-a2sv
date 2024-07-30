278696ce368174e891983d0ee0d769767a1a74af
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-07-30 20:31:35 +0300

    Created 'C - Removal Hack' by A2SV Hub

---
+++ A2SV G5 - Contest #24 30-Jul-2024/C - Removal Hack 185973.py
@@ -0,0 +1,26 @@
+# Problem: C - Removal Hack - https://codeforces.com/gym/534160/problem/C
+
+n = int(input())
+tree = [[] for _ in range(n + 1)]
+count = [0 for _ in range(n + 1)]
+ct = [0 for _ in range(n + 1)]
+
+
+
+for i in range(1, n + 1):
+    p, c = map(int, input().split())
+    
+    if p != -1:
+        tree[p].append(i)
+        count[p] += 1 - c
+    ct[i] = 1 - c
+
+# print(tree)
+# print(count)
+# print(ct)
+a = [i for i in range(1, n + 1) if count[i] == 0 and ct[i] == 0]
+a.sort()
+if len(a) == 0:
+    print(-1)
+else:
+    print(*a)
