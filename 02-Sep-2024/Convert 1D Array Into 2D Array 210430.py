3d42bc46e58a7cea6a8c0e7cf260c4668b5eba5b
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-09-02 19:53:03 +0300

    Created 'Convert 1D Array Into 2D Array' by A2SV Hub

---
+++ 02-Sep-2024/Convert 1D Array Into 2D Array 210430.py
@@ -0,0 +1,12 @@
+# Problem: Convert 1D Array Into 2D Array - https://leetcode.com/problems/convert-1d-array-into-2d-array/
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return []
+        
+        ans = []
+        for i in range(m):
+            ans.append(original[n * i: (i + 1) * n])
+
+        return ans
