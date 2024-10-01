7f1cc5a780d645f34e85018891e7c1c915de0c05
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-10-01 22:03:28 +0300

    Created 'Find Missing Observations' by A2SV Hub

---
+++ 01-Oct-2024/Find Missing Observations 217378.py
@@ -0,0 +1,15 @@
+# Problem: Find Missing Observations - https://leetcode.com/problems/find-missing-observations/
+
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        missing = mean * (n + m) - sum(rolls)
+        
+        if missing < n or ceil(missing / n) > 6:
+            return []
+
+        res = [missing // n for _ in range(n)]
+        for i in range(missing % n):
+            res[i] += 1
+
+        return res
