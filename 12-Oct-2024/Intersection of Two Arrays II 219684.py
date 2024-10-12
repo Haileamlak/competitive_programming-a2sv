cf2c1169ec4b72860fed75fd0d6a3ae96c241174
Haileamlak Belachew <haileamlakbeat@gmail.com>
2024-10-12 21:26:31 +0300

    Created 'Intersection of Two Arrays II' by A2SV Hub

---
+++ 12-Oct-2024/Intersection of Two Arrays II 219684.py
@@ -0,0 +1,24 @@
+# Problem: Intersection of Two Arrays II - https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        max1 = max(nums1)
+        count1 = [0 for _ in range(max1 + 1)] 
+        for num in nums1:
+            count1[num] += 1
+        
+        max2 = max(nums2)
+        count2 = [0 for _ in range(max2 + 1)]
+        for num in nums2:
+            count2[num] += 1
+
+        res = []
+        length = min(max1, max2)
+        for i in range(length + 1):
+            j = min(count1[i], count2[i])
+            while j > 0:
+                res.append(i)
+                j -= 1
+        
+        return res
